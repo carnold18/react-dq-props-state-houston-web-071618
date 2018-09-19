e5e7d0fd8daed7c43453ae1143e85509318ac9f2
Christopher Arnold <chrisarnold2010@gmail.com>
2018-09-19 10:48:58 -0500

    Fully functional

--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react'
 import Master from './assets/master-hog.png'
 import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import offspring from './db.js'
 
-export default class MasterHog extends Component {
+// Naming conventions are VITAL - camelCasing (functions/arrays) versus PascalCasing (components)
+
+class MasterHog extends Component {
 
   constructor() {
     super()
     this.state = {
-      eyeColor: "blue",
+      eyeColor: "",
     }
   }
+  // THINK ABOUT MODULE 2 RELATIONSHIPS (one to many and table relationships)
 
+  // When do we use a state?
+  // You use a state if you aren’t inheriting properties from the parent. Is the attribute passed down 
+  // from the parent component? If not, initiate it with a state (can set the state to an empty string if you want).
+
+  // When do we use a prop?
+  // You use props if properties are “propogating” down from the the parent. 
+
+  listOffspring = () => {
+    return offspring.map((offspring) => {
+      return <BabyHog eyeColor={this.state.eyeColor}  {...offspring}/>
+    })
+  }
+
+  // 1. Create a function that you can call in the render below.
+  // 2. Pass the imported array.
 
   changeEyeColor = (e) => {
     this.setState({
       eyeColor: e.target.value
     })
   }
-
 
   render() {
     return (
@@ -40,13 +57,13 @@ export default class MasterHog extends Component {
         </div>
         
         <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
+          {this.listOffspring()}
         </ul>
-
       </div>
+      // Remember on Line 51 to return the function, using the (). Without this, we are only passing the function
+      // istelf into the ul.
     )
   }
-
 }
+
+export default MasterHog;
